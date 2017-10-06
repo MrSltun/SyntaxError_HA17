3834895428dadf9422dd71dcfb4befd1bccfcc79
فهد <a-bagazi@hotmail.com>
2017-10-06 14:21:09 +0300

    Main.js UI done

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,18 +1,37 @@
-/* @flow */
-
 import React, { Component } from 'react';
 import {
   View,
-  Text,
   StyleSheet,
+  Image
 } from 'react-native';
+import { Container, Header, Content, Button, Text } from 'native-base';
+
 
 export default class Main extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Im the Main component</Text>
-      </View>
+      <Container>
+
+    <View style={styles.containerImg}>
+    <Image
+          source={require('../img/home.jpg')}
+          style={{width: 100, height: 100}}
+
+        />
+        </View>
+
+          <View style={styles.container}>
+            <Button style={styles.button}>
+              <Text>Click Me! </Text>
+            </Button>
+            <Button style={styles.button}>
+              <Text >Click Me! </Text>
+            </Button>
+            <Button style={styles.button}>
+              <Text >Click Me! </Text>
+            </Button>
+          </View>
+        </Container>
     );
   }
 }
@@ -20,5 +39,33 @@ export default class Main extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignSelf: 'center'
+
+
+
+
   },
+  button:{
+  marginBottom: 10,
+  width: 150,
+  justifyContent: 'center',
+
+
+
+
+},
+containerImg:{
+
+
+alignSelf: 'center',
+marginBottom: -200,
+marginTop: 150
+
+
+
+},
+
+
 });
