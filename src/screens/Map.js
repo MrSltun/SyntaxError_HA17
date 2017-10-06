d2ab01c589c588c10e3310ce4c53d681739d9d6a
فهد <a-bagazi@hotmail.com>
2017-10-07 02:49:46 +0300

    map has been fixed need to add marker

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,98 +1,222 @@
-/* @flow */
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+} from 'react-native';
+
 import MapView from 'react-native-maps';
 
-import React, { Component } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-} from 'react-native';
-import { Container, Header, Left, Body, Icon, Title, Button} from 'native-base';
-import { Link } from 'react-router-native'
+const { width, height } = Dimensions.get('window');
 
-export default class Map extends Component {
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+const customStyle = [
+  {
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#242f3e',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#746855',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#242f3e',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.locality',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#d59563',
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#d59563',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#263c3f',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#6b9a76',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#38414e',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#212a37',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#9ca5b3',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#746855',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#1f2835',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#f3d19c',
+      },
+    ],
+  },
+  {
+    featureType: 'transit',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#2f3948',
+      },
+    ],
+  },
+  {
+    featureType: 'transit.station',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#d59563',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#17263c',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#515c6d',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#17263c',
+      },
+    ],
+  },
+];
+
+class MapStyle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
   render() {
-    const { region } = this.props;
-    console.log(region);
-
     return (
-         <View style ={styles.container}>
-
-         <Header>
-           <Left>
-             <Button transparent>
-             <Link to="/">
-                           <Icon name='arrow-back' />
-                           </Link>
-             </Button>
-           </Left>
-           <Body>
-             <Title>Items</Title>
-           </Body>
-         </Header>
-
-
-           <MapView
-             style={styles.map}
-             region={{
-               latitude: 37.78825,
-               longitude: -122.4324,
-               latitudeDelta: 0.015,
-               longitudeDelta: 0.0121,
-             }}
-           >
-
-           <MapView.Marker coordinate={{latitude: 37.78825, longitude: -122.4324}}>
-           </MapView.Marker>
-
-
-           </MapView>
-
-           <View style={styles.view3}>
-         <Button block light style={styles.Button}>
-           <Text>Dark</Text>
-         </Button>
-
-       <Button block light style={styles.Button}>
-       <Link to="/">
-
-         <Text>menu</Text>
-
-         </Link>
-       </Button>
-
-       </View>
-         </View>
-
-
-       );
+      <View style={styles.container}>
+        <MapView
+          provider={this.props.provider}
+          style={styles.map}
+          initialRegion={{
+            latitude: LATITUDE,
+            longitude: LONGITUDE,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }}
+          customMapStyle={customStyle}
+        />
+      </View>
+    );
   }
 }
+
+MapStyle.propTypes = {
+  provider: MapView.ProviderPropType,
+};
 
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
   map: {
     ...StyleSheet.absoluteFillObject,
   },
-  view3:{
-flex: 0.5,
-flexDirection: 'row',
-justifyContent:'space-around',
-paddingTop: 2000
-
-
-
-  },
-  Button:{
-flex: 1,
-maxWidth: 100
-
-  },
-
 });
+
+module.exports = MapStyle;
