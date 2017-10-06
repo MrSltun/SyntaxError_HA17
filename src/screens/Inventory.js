b4a08064168eb0381abe54a8409edbdb2b59d332
فهد <a-bagazi@hotmail.com>
2017-10-06 17:14:24 +0300

    inventry.js UI done

--- src/screens/Inventory.js
+++ src/screens/Inventory.js
@@ -3,19 +3,66 @@
 import React, { Component } from 'react';
 import {
   View,
-  Text,
   StyleSheet,
+  TouchableHighlight,
+  Image
 } from 'react-native';
 import {inject, observer} from 'mobx-react/native'
+import { Container, Header, Content, Left, Right, Icon, Title, Button, List, ListItem, Thumbnail, Text, Body } from 'native-base';
 
 @inject('store') @observer
 export default class Inventory extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>I'm the Inventory component</Text>
-        <Text>{this.props.store.example.data}</Text>
-      </View>
+      <View>
+
+      <Header>
+        <Left>
+          <Button transparent>
+            <Icon name='arrow-back' />
+          </Button>
+        </Left>
+        <Body>
+          <Title>Items</Title>
+        </Body>
+        <Right>
+
+        </Right>
+      </Header>
+
+          <List  style={styles.list}>
+            <ListItem style={styles.body}>
+              <Body>
+                <Text>Sankhadeep</Text>
+                <Text note>Its time to build a difference . .</Text>
+              </Body>
+            </ListItem>
+
+            <ListItem style={styles.body}>
+              <Body>
+                <Text>Sankhadeep</Text>
+                <Text note>Its time to build a difference . .</Text>
+              </Body>
+            </ListItem>
+
+            <ListItem style={styles.body}>
+              <Body>
+                <Text>Sankhadeep</Text>
+                <Text note>Its time to build a difference . .</Text>
+              </Body>
+            </ListItem>
+
+            <ListItem style={styles.body}>
+              <Body>
+                <Text>Sankhadeep</Text>
+                <Text note>Its time to build a difference . .</Text>
+              </Body>
+            </ListItem>
+          </List>
+
+
+
+</View>
     );
   }
 }
@@ -24,4 +71,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+
+  list:{
+
+   justifyContent: 'center',
+   width: 400,
+   paddingTop: 30
+
+ },
+ body: {
+
+   margin: 5
+
+
+
+ }
+
 });
