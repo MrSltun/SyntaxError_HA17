a34811b1ac8cd19c4b97b66afeda3b2a64420f85
abdulrhman bagazi <a-bagazi@hotmail.com>
2017-10-12 01:15:24 +0300

    main UI

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -5,7 +5,7 @@ import {
   Image,
   Dimensions
 } from 'react-native';
-import { Container, Header, Content, Button, Text } from 'native-base';
+import { Container, Header, Content, Button, Text, Body, Title, Left, Icon, Right, Footer, FooterTab} from 'native-base';
 import {Link} from 'react-router-native'
 import axios from 'axios'
 const { width } = Dimensions.get('window')
@@ -14,23 +14,41 @@ export default class Main extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.containerImg}>
-          <Image
-            source={require('../img/home.png')}
-            style={{ resizeMode: 'contain', width: width / 2 , height: width / 2 }} />
-        </View>
-        <View style={{...styles.container, justifyContent: 'center'}}>
-          <Button style={styles.button}>
-            <Link to="/Camera">
-              <Text>ابدأ المسح</Text>
-            </Link>
-          </Button>
-          <Button style={styles.button}>
-            <Link to="/Inventory">
-              <Text>المخزن</Text>
-            </Link>
-          </Button>
-        </View>
+
+
+
+               <View style={styles.boxOne}>
+
+               <Image
+                           source={require('../img/icon.png')}
+                           style={{ resizeMode: 'contain', width: width / 2 , height: width / 2 }} />
+
+               </View>
+
+
+
+
+
+         <View style={styles.boxTwo}>
+
+
+
+
+         <Button rounded style={styles.button}>
+           <Text>Start Scanning</Text>
+         </Button>
+
+         <Button rounded style={styles.button}>
+           <Text>Inventory</Text>
+         </Button>
+
+         </View>
+
+
+
+
+
+
       </View>
     );
   }
@@ -39,20 +57,48 @@ export default class Main extends Component {
 const styles = {
   container: {
     flex: 1,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    backgroundColor: '#E8F5E9'
+
+
   },
 
-  button:{
-    margin: 7,
-    width: 150,
-    justifyContent: 'center'
+
+  boxcontainerOne: {
+    flex: 1,
+    flexDirection: 'column'
+
+
+
   },
 
-  containerImg:{
-    flex: 1.3,
-    alignSelf: 'center',
-    justifyContent: 'center'
-  }
+  boxOne: {
+   flex: 2,
+   backgroundColor: '#fafafa',
+   justifyContent: 'center',
+   alignItems: 'center',
+
+
+ },
+
+
+  boxTwo: {
+   flex: 1,
+   backgroundColor: '#fafafa',
+   alignItems: 'center',
+   justifyContent: 'center',
+
+ },
+
+
+
+
+button:{
+
+alignSelf: 'center',
+marginBottom: 20
+
+
+
+},
+
+
 }
